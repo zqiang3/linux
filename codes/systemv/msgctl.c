36be334155f97d686349d8057daa0468c8ce42dc
zqiang3 <zqiang3@126.com>
2018-08-01 23:13:49 -0700

    remove mqueue

--- codes/systemv/msgctl.c
+++ codes/systemv/msgctl.c
@@ -1,22 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <linux/ipc.h>
-#include <linux/msg.h>
-#include <sys/types.h>
+#include "msg.h"
 
-struct msgmbuf{
-    int mtype;
-    char mtext[10];
-};
 
 
 void remove_queue(int);
-void err_quit(const char*);
-void println(char *ptr)
-{
-    printf("%s\n", ptr);
-}
 
 int main(int argc, char **argv)
 {
@@ -27,33 +13,24 @@ int main(int argc, char **argv)
         exit(1);
         
     }
-    else
-    {
-        char *path = argv[1];
-        int type = atoi(argv[2]);
-        key_t key = ftok(path, 'b');
-        int open_mode = 0666 | IPC_CREAT;
-        struct msgmbuf buf;
-        struct msqid_ds info;
-        int msqid = msgget(key, open_mode);
-        printf("msqid: %d, key: %x\n", msqid, key);
+
+    char *path = argv[1];
+    int type = atoi(argv[2]);
+    int mqid = open_msg(path);
+    if(mqid < 0)
+        err_quit("open msg fail");
         
         //msgctl(msqid, IPC_STAT, &info);
         //printf("read-write:%d, cbytes: %d, qnum=%d, qbytes=%d\n",
         //info.msg_perm.mode & 0777, info.msg_cbytes,
         //info.msg_qnum, info.msg_qbytes);
 
+    if(strcmp(argv[2], "remove") == 0)
+        remove_queue(mqid);
 
-        if(strcmp(argv[2], "remove") == 0)
-            remove_queue(msqid);
+    printf("here\n");
 
-        printf("here\n");
-
-        return 0;
-        
-    }
-
-
+    return 0;
 }
 
 void remove_queue(int mqid)
@@ -69,8 +46,3 @@ void remove_queue(int mqid)
 }
 
 
-void err_quit(const char *info)
-{
-    printf("%s\n", info);
-    exit(1);
-}
