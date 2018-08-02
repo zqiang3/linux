4ab9b2c1e55d417df8b1660e82ae179d84fa39e1
zqiang3 <zqiang3@126.com>
2018-08-01 22:40:57 -0700

    send msg

--- codes/systemv/msgsnd.c
+++ codes/systemv/msgsnd.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <errno.h>
-#include <linux/ipc.h>
-#include <linux/msg.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+struct msgmbuf{
+    int mtype;
+    char mtext[10];
+};
 
 void err_quit(const char*);
-void println(char *ptr)
+void println(const char *ptr)
 {
     printf("%s\n", ptr);
 }
@@ -14,38 +20,45 @@ int main(int argc, char **argv)
 {
     if(argc < 4)
     {
-        printf("arc less than 4");
+        printf("Usage: send <path> <type> <info>");
         exit(1);
         
     }
+
+    int ret;
+    char *path = argv[1];
+    int type = atoi(argv[2]);
+    char *info = argv[3];
+    key_t key = ftok(path, 'b');
+    if(key < 0)
+        err_quit("ftok failed");
     else
+        printf("key: 0x%x\n", key);
 
-    {
-        char *path = argv[1];
-        int len = atoi(argv[2]);
-        int type = atoi(argv[3]);
-        key_t key = ftok(path, 1);
-        int open_mode = 0666 | IPC_CREAT;
-        int mqid = msgget(key, open_mode);
+    int open_mode = 0666 | IPC_CREAT;
+    int mqid = msgget(key, open_mode);
+    if(mqid < 0)
+        err_quit("open mqueue failed");
+    else
+        printf("mqid: %d\n", mqid);
 
-        int calloc_size = sizeof(long) + len;
-        struct msgbuf *ptr;
-        struct msgbuf buff1;
-        buff1.mtype = 1;
-        ptr = (struct msgbuf *)calloc(calloc_size, sizeof(char));
-        ptr->mtype = type;
+    struct msgmbuf buff;
+    buff.mtype = 1;
+    int msg_len = strlen(info) + 1;
+    memcpy(buff.mtext, info, msg_len);
+    ret = msgsnd(mqid, &buff, msg_len, 0);
+    if(ret < 0)
+        err_quit("send msg failed");
+    else
+        println("send msg success");
 
-        msgsnd(mqid, ptr, len, 0);
-        return 0;
-        
-    }
-
-
+    return 0;
 }
 
 
 void err_quit(const char *info)
 {
-    printf("%s\n", info);
+    perror(info);
     exit(1);
 }
+
