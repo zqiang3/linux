43387260398289d2789cf0c74831033afb315f5f
zqiang3 <zqiang3@126.com>
2018-08-07 05:22:57 -0700

    semaphore

---
+++ codes/semphore/wait.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <semaphore.h>
+
+int main(int argc, char **argv)
+{
+    if(argc < 2)
+    {
+        printf("usage: cmd <name>\n");
+        exit(1);
+    }
+
+    int flags;
+    sem_t *sem;
+    unsigned int value;
+    value = 1;
+
+    char *path = argv[1];
+    flags = O_RDWR | O_CREAT;
+    sem = sem_open(argv[1], flags, 0666, value);
+
+    int val;
+    sem_wait(sem);
+    sem_getvalue(sem, &val);
+    printf("sem val: %d\n", val);
+
+    sem_close(sem);
+
+    return 0;
+}
