4da07faadef4a1efe08ebbc9f3eaae168c8b4033
zqiang3 <zqiang3@126.com>
2018-08-08 02:37:26 -0700

    producer consumer

--- codes/semphore/prodcons3.c
+++ codes/semphore/prodcons3.c
@@ -5,20 +5,16 @@
 #include <time.h>
 
 #define NBUFF 10
-#define MAX_THREADS 100
 #define SEM_MUTEX  "mutex"
 #define SEM_NEMPTY "empty"
 #define SEM_NSTORED "stored"
 
 struct {
-    int nput;
-    int nputval;
     int buff[NBUFF];
     sem_t mutex, nempty, nstored;
 } shared;
 
 int nitems;
-int nthreads;
 
 void *produce(void *arg);
 void *consume(void *arg);
@@ -26,17 +22,13 @@ void *consume(void *arg);
 
 int main(int argc, char **argv)
 {
-    if(argc < 3)
+    if(argc < 2)
     {
-        printf("usage: cmd <items> <threads>\n");
+        printf("usage: cmd <items>\n");
         exit(1);
     }
 
     nitems = atoi(argv[1]);
-    nthreads = atoi(argv[2]);
-
-    shared.nput = 0;
-    shared.nputval = 0;
 
     int flag = O_CREAT;
     int mode = 0666;
@@ -45,23 +37,10 @@ int main(int argc, char **argv)
     sem_init(&shared.nempty, 0, NBUFF);
     sem_init(&shared.nstored, 0, 0);
 
-    pid_t tid_consume;
-    pid_t tid_produce[nthreads];
-    int count[nthreads];
-    int i;
-    for(i = 0; i < nthreads; i++)
-    {
-        count[i] = 0;
-        pthread_create(&tid_produce[i], NULL, produce, &count[i]);
-        
-    }
+    pid_t tid_produce, tid_consume;
+    pthread_create(&tid_produce, NULL, produce, NULL);
     pthread_create(&tid_consume, NULL, consume, NULL);
-
-    for(i = 0; i < nthreads; i++)
-    {
-        pthread_join(tid_produce[i], NULL);
-        printf("count[%d] = %d\n", i, count[i]);
-    }
+    pthread_join(tid_produce, NULL);
     pthread_join(tid_consume, NULL);
 
     sem_destroy(&shared.mutex);
@@ -76,26 +55,15 @@ void *produce(void *arg)
     int i;
     struct timeval now;
     int val_mutex, val_nempty, val_nstored;
-    int *count = (int *)arg;
-    for(;;)
+    for(i = 0; i < nitems; i++)
     {
         sem_wait(&shared.nempty);
         sem_wait(&shared.mutex);
-        if(shared.nput >= nitems)
-        {
-            sem_post(&shared.mutex);
-            sem_post(&shared.nempty);
-            break;
-        }
 
-        printf("put i=%d to buff\n", shared.nputval);
+        printf("put i=%d to buff\n", i);
         gettimeofday(&now, NULL);
         //printf("time: %ld %ld\n", now.tv_sec, now.tv_usec);
-        shared.buff[shared.nput % NBUFF] = shared.nputval;
-        shared.nput++;
-        shared.nputval++;
-        (*count)++;
-
+        shared.buff[i % NBUFF] = i;
         
         sem_post(&shared.mutex);
         sem_post(&shared.nstored);
