022d617d1df68fd2d07882efbe73419b2516408d
zqiang3 <zqiang3@126.com>
2018-11-10 17:41:09 -0800

    make numbers

---
+++ codes/ds/make_numbers.py
@@ -0,0 +1,34 @@
+import random
+import sys
+import struct
+MAX = 10000000
+
+def write_numbers(filename):
+    f = open(filename, 'wb')
+
+    for i in xrange(MAX):
+        num = random.randint(1, sys.maxint)
+        b_num = struct.pack('i', num)
+        f.write(b_num)
+
+    f.close()
+
+def write_numbers_str(filename):
+    f = open(filename, 'w')
+    for i in xrange(MAX):
+        num = random.randint(1, sys.maxint)
+        f.write(str(num))
+        f.write('\n')
+
+def read_numbers(filename):
+    f = open(filename, 'rb')
+    b_num = f.read(4)
+    ret = struct.unpack('i', b_num)
+    num = ret[0]
+    print 'read num', num
+    f.close()
+
+filename = 'numbers.txt'
+write_numbers(filename)
+#read_numbers(filename)
+
